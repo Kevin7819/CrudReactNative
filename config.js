f1f964ed9482ceed8e4cddf74d9278ad48226627
gonzalodr <129683931+gonzalodr@users.noreply.github.com>
2025-05-30 17:23:08 -0600

    updatee-breanch
    - .env config
    - frebase config
    - action aply
    - app config

--- config.js
+++ config.js
@@ -1,16 +1,15 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import Constants from "expo-constants";
+import Constants from 'expo-constants';
 
 const firebaseConfig = {
-    apiKey: Constants.manifest.extra.apiKey,
-    authDomain: Constants.manifest.extra.authDomain,
-    projectId: Constants.manifest.extra.projectId,
-    storageBucket: Constants.manifest.extra.storageBucket,
-    messagingSenderId: Constants.manifest.extra.messagingSenderId,
-    appId: Constants.manifest.extra.appId,
-    databaseURL: Constants.manifest.extra.databaseURL
+  apiKey: Constants.expoConfig.extra.FIREBASE_API_KEY,
+  authDomain: Constants.expoConfig.extra.FIREBASE_AUTH_DOMAIN,
+  projectId: Constants.expoConfig.extra.FIREBASE_PROJECT_ID,
+  storageBucket: Constants.expoConfig.extra.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: Constants.expoConfig.extra.FIREBASE_MESSAGING_SENDER_ID,
+  appId: Constants.expoConfig.extra.FIREBASE_APP_ID,
 };
 
-initializeApp(firebaseConfig);
-export const database = getFirestore();
+const app = initializeApp(firebaseConfig);
+export const database = getFirestore(app);
